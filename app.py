07a927a921d63e8e537c70fc4a93f3461d70bcbd
Doremon <www.saylipatil1603@gmail.com>
2022-12-15 21:03:33 +0530

    ..

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/greet", methods=["POST"])
+def greet():
+    name = request.args.get("name", "world")
+    return render_template("greet.html", name=name)
